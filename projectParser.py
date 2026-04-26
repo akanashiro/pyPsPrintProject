0be528671fcaa0e752c22a3bafcbf9c0fbf72361
kanashirosan <47354267+akanashiro@users.noreply.github.com>
2026-04-26 01:20:12 -0400

    fix import in projectParser.py

--- projectParser.py
+++ projectParser.py
@@ -17,7 +17,7 @@ Requisites:
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass, field
 from typing import Optional
-import helperFunctions as helpers
+import helpFunctions as helpers
 
 
 @dataclass
